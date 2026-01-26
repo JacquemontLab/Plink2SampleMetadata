7f62755d77772930ee6e2355a51d27c5bb8cd883
Bénitière Florian <34925751+FloBeni@users.noreply.github.com>
2026-01-26 15:10:09 -0500

    Modify cleanup process in extraction_king_ref.sh
    Removed the deletion of *_update* files from cleanup.

--- resources/king_ref/extraction_king_ref.sh
+++ resources/king_ref/extraction_king_ref.sh
@@ -117,7 +117,8 @@ if [[ "$genome_version" == "GRCh38" ]]; then
     plink --bfile KGref_GRCh38_filter_update_position --update-name name_to_update.tsv --make-bed --out KGref_GRCh38_final --memory ${mem_MB} --threads ${cpus}
 fi
 
-rm *_intermediate* *_filter* *_update* hg19ToHg38.over.chain.gz KGref_unmapped.bed KGref_GRCh37_map.bed KGref_GRCh38_SNP_Chr_map.tsv KGref_GRCh38_SNP_pos_map.tsv KGref_GRCh38_SNP_selection.txt
+rm *_intermediate* *_filter* hg19ToHg38.over.chain.gz KGref_unmapped.bed KGref_GRCh37_map.bed KGref_GRCh38_SNP_Chr_map.tsv KGref_GRCh38_SNP_pos_map.tsv KGref_GRCh38_SNP_selection.txt
+rm *_update*
 
 ######### TO CHECK GENOME REFERENCE
 
